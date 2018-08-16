d908acf767869853e11233872740a257d3132852
Nicolas osorio <nicolas.osorio@mail.escuelaing.edu.co>
2018-08-15 19:10:29 -0500

    Primer Commit de trabajo

--- angular-seed/src/app/pages/task-edit-page/task-edit-page.component.ts
+++ angular-seed/src/app/pages/task-edit-page/task-edit-page.component.ts
@@ -1,18 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../todo.service';
+import { Todo } from '../../todo';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-edit-page',
   templateUrl: './task-edit-page.component.html',
   styleUrls: ['./task-edit-page.component.css']
 })
-export class TaskEditPageComponent implements OnInit {
-  constructor(
-  ) {
+export class TaskEditPageComponent implements OnInit
+{
+	    private todoForm;
 
-  }
+        constructor(public todoService: TodoService,
+		public formBuilder: FormBuilder,
+    	public router: Router){
+
+		}
+
+  
 
   ngOnInit() {
+	  this.todoForm = this.formBuilder.group({
+      description: '',
+      completed: '',
+      priority: ''
+    });
+  }
 
+  onSubmit() {
+    this.todoService.create(
+      this.todoForm.get('description').value,
+      this.todoForm.get('priority').value,
+      Boolean(this.todoForm.get('completed').value)
+   );
+
+   this.router.navigate(['/tasks']);
   }
 
 }
